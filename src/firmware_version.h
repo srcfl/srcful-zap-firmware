452dfee9b77287884669814e8cb25ef8f947158c
h0bb3 <tobias.ohlsson@gmail.com>
2025-05-22 15:21:06 +0200

    just a firmware patch bump

--- src/firmware_version.h
+++ src/firmware_version.h
@@ -3,7 +3,7 @@
 // Firmware version information
 #define FIRMWARE_VERSION_MAJOR 0
 #define FIRMWARE_VERSION_MINOR 0
-#define FIRMWARE_VERSION_PATCH 10
+#define FIRMWARE_VERSION_PATCH 11
 
 // Helper macro to create version string
 #define STRINGIFY(x) #x
